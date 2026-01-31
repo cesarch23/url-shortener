9d40dd5caf1f8b1dee124721d015495ff664d4b4
Cesar Choccata <c21choccata@gmail.com>
2026-01-31 18:45:26 -0500

    Upload project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/service/theme.service';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavBarComponent,
+    RouterOutlet, 
+    MatButtonModule, 
+    MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'url-shortener';
+  constructor( public theme: ThemeService){
+    this.theme.enableDark()
+  }
+  toggleTheme() {
+    this.theme.toggle();
+  }
+
 }
