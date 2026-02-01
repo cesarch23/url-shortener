68c9973a0739dc3c2d76a7fddf43307f445f5d07
Cesar Choccata <c21choccata@gmail.com>
2026-02-01 17:20:07 -0500

    Add  home component

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @Component({
   selector: 'dashboard-home',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
