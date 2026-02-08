d9b9d9394e0a5c56570d2a05a2dfd47a9439cb37
Cesar Choccata <c21choccata@gmail.com>
2026-02-08 10:42:20 -0500

    Add redirect guard

---
+++ src/app/core/guard/redirect.guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '../service/token.service';
+
+export const redirectGuard: CanActivateFn = (route, state) => {
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+  if(tokenService.isValidToken()){
+    tokenService.redirectBasedRole();
+    return false;
+  }
+  router.navigateByUrl("/")//login
+  return true;
+};
