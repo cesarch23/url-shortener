333b895f042377ff743eed669db5fd0352c82c1a
Cesar Choccata <c21choccata@gmail.com>
2026-02-08 10:22:41 -0500

    Update gurads to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,28 @@
 import { Routes } from '@angular/router';
+import { Oauth2RedirectComponent } from './shared/components/oauth2-redirect/oauth2-redirect.component';
+import { LandingComponent } from './landing/landing.component';
+import { authGuard } from './core/guard/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
         //verificar autenticacion
-        loadComponent: () => import('./landing/landing.component').then(m => m.LandingComponent)
+        component: LandingComponent
 
     },
     {
+        path: 'oauth2/redirect',
+        component: Oauth2RedirectComponent
+    },
+    {
         path: 'dashboard',// usuario autenticados
+        canActivate: [authGuard],
         loadChildren: ()=> import('./dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES)
         
     },
     {
         path: 'admin',//administradores de la empressas
+        canActivate: [authGuard],
         loadChildren: ()=> import('./admin/admin.routes').then(m => m.ADMIN_ROUTES)
     },
     {
