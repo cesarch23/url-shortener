faa7bfae562f6f9e3c29f828c18bb0c2eabd505f
Cesar Choccata <c21choccata@gmail.com>
2026-02-08 10:19:19 -0500

    add auth guard

---
+++ src/app/core/guard/auth.guard.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { TokenService } from '../service/token.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+  if(tokenService.isValidToken()) return true;
+  else{
+    router.navigateByUrl("/")
+    return false
+  };
+};
