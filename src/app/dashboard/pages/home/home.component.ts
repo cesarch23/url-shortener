119671e4b48dca26a8af7f311095da8fa0e870e8
Cesar Choccata <c21choccata@gmail.com>
2026-02-08 23:32:34 -0500

    Add home component

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../../core/service/auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltip } from "@angular/material/tooltip";
+import { RouterLink } from "@angular/router";
+import {MatRippleModule} from '@angular/material/core';
 @Component({
   selector: 'dashboard-home',
   imports: [
@@ -10,11 +15,17 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-
-  ],
+    MatIconModule,
+    MatTooltip,
+    MatRippleModule,
+    RouterLink
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  
+  private authService = inject(AuthService);
+  readonly currentUser = this.authService.currentUser$;
 
 }
