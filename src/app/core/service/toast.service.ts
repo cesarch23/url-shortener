2dc6af38da55d1eccab21dbd4c1a320686b1360b
Cesar Choccata <c21choccata@gmail.com>
2026-02-09 12:36:50 -0500

    Add types for opensnack params

--- src/app/core/service/toast.service.ts
+++ src/app/core/service/toast.service.ts
@@ -1,51 +1,80 @@
 import { inject, Injectable } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ToastParams } from '../model/model.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToastService {
-
   private _snackBar = inject(MatSnackBar);
 
   private openSnackBar(
-    message:string,
-    action:string,
-    duration:number,
-    type: 'alert' | 'error' |'success' | 'info',
+    message: string,
+    action: string,
+    duration: number,
+    type: 'alert' | 'error' | 'success' | 'info',
     horizontalPosition: 'start' | 'center' | 'end' | 'left' | 'right',
-    verticalPosition:'top' | 'bottom'
-   ){
-    this._snackBar.open(message,action,{
-        horizontalPosition: horizontalPosition,
-        verticalPosition: verticalPosition,
-        duration:duration
-      });
+    verticalPosition: 'top' | 'bottom',
+  ) {
+    this._snackBar.open(message, action, {
+      horizontalPosition: horizontalPosition,
+      verticalPosition: verticalPosition,
+      duration: duration,
+    });
   }
-  showToast(
-    message:string,
-    action:string = 'ok',
-    duration:number=10000,//10 segundos -> en ms
-    type: 'alert' | 'error' |'success' | 'info' = 'info',
-    horizontalPosition: 'start' | 'center' | 'end' | 'left' | 'right' = 'center',
-    verticalPosition:'top' | 'bottom' = 'bottom'
-  ){
-
-    if(type === 'alert' ){
-      this.openSnackBar(message,action,duration,type,horizontalPosition,verticalPosition)
+  showToast({
+    message,
+    action = 'ok',
+    duration = 10000,
+    type = 'info',
+    horizontalPosition = 'center',
+    verticalPosition = 'bottom',
+  }: ToastParams) {
+    if (type === 'alert') {
+      this.openSnackBar(
+        message,
+        action,
+        duration,
+        type,
+        horizontalPosition,
+        verticalPosition,
+      );
+    } else if (type === 'error') {
+      this.openSnackBar(
+        message,
+        action,
+        duration,
+        type,
+        horizontalPosition,
+        verticalPosition,
+      );
+    } else if (type === 'success') {
+      this.openSnackBar(
+        message,
+        action,
+        duration,
+        type,
+        horizontalPosition,
+        verticalPosition,
+      );
+    } else if (type === 'info') {
+      this.openSnackBar(
+        message,
+        action,
+        duration,
+        type,
+        horizontalPosition,
+        verticalPosition,
+      );
+    } else {
+      this.openSnackBar(
+        message,
+        action,
+        duration,
+        type,
+        horizontalPosition,
+        verticalPosition,
+      );
     }
-    else if(type === 'error'){
-      this.openSnackBar(message,action,duration,type,horizontalPosition,verticalPosition)
-    }
-    else if(type === 'success'){
-      this.openSnackBar(message,action,duration,type,horizontalPosition,verticalPosition)
-    }
-    else if(type === 'info'){
-      this.openSnackBar(message,action,duration,type,horizontalPosition,verticalPosition)
-    }
-    else{
-      this.openSnackBar(message,action,duration,type,horizontalPosition,verticalPosition)
-    }
-
   }
 }
