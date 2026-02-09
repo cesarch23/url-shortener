5e2c70f27d4ed35a122d4947e31924abf32fc502
Cesar Choccata <c21choccata@gmail.com>
2026-02-09 12:37:10 -0500

    Add arrays links

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -8,6 +8,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltip } from "@angular/material/tooltip";
 import { RouterLink } from "@angular/router";
 import {MatRippleModule} from '@angular/material/core';
+import { ToastService } from '../../../core/service/toast.service';
+import { ToastParams } from '../../../core/model/model.interface'
 @Component({
   selector: 'dashboard-home',
   imports: [
@@ -26,6 +28,95 @@ import {MatRippleModule} from '@angular/material/core';
 export class HomeComponent {
   
   private authService = inject(AuthService);
+  private toastService = inject(ToastService);
   readonly currentUser = this.authService.currentUser$;
+  links = [
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://developer.mozilla.org/es/docs/Web/CSS/Reference/Properties/flex"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+    {
+      shorLink: "https://v19.mate/12345678",
+      longUrl: "https://www.google.com/search?q=search+with+algolia&oq=search+with+algolia&gs_lcrp=EgZjaHJvbWUyCggAEEUYFhgeGDkyCAgBEAAYFhgeMggIAhAAGBYYHjIICAMQABgWGB4yCAgEEAAYFhgeMggIBRAAGBYYHjIICAYQABgWGB4yCAgHEAAYFhgeMggICBAAGBYYHjIKCAkQABgKGBYYHtIBCDczODFqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8"
+    },
+  ]
+
+  public async copyShorLink(shorLink:string){
+    try{
+      await navigator.clipboard.writeText(shorLink);
+      this.toastService.showToast({message: "Enlace copiado al portapapeles",type:'info',horizontalPosition:'center',duration: 2000})
+    }
+    catch(error){
+      this.toastService.showToast({message: "No se pudo copiar al portapapeles",type:'error'})
+    }
+  } 
 
 }
