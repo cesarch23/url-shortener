d7af8cd1d2f2c75232a4c689eae21be24df70877
Cesar Choccata <c21choccata@gmail.com>
2026-02-11 17:31:10 -0500

    Add provideNativeDateAdapter for dates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptor/token.interceptor';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 
 export const appConfig: ApplicationConfig = {
@@ -13,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([tokenInterceptor]),
       withFetch()
-    )
+    ),
+    provideNativeDateAdapter()
   ]
 };
